3068227b16c4053d2657c8df1ccd04b68af49b58
leandroDavid <leandroDavid@192.168.1.5>
2017-04-21 10:13:22 -0300

    Roterização inicial

---
+++ src/java/Controle/InserirCargo.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import Entidade.Cargo;
+import Hibernate.HibernateUtil;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author leandroDavid
+ */
+public class InserirCargo extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+                 try{
+                Cargo CrCargo = new Cargo();
+
+                CrCargo.setPerfil(request.getParameter("cargo"));
+                CrCargo.setIdCargo(1);
+                /*
+                String num = request.getParameter("num");
+                if(num != null){
+                    Integer numi = Integer.parseInt(num);
+                    cargo.setNum_serie(numi);
+                }
+*/ 
+                Session sessao = HibernateUtil.getSessionFactory().openSession();
+                     out.println(CrCargo.getPerfil());
+
+                Transaction tx = sessao.beginTransaction();
+
+                sessao.save(CrCargo);
+                sessao.flush();
+
+                tx.commit();
+
+                sessao.close();
+                
+                out.println("Regist ro inserido com sucesso!");
+            } catch (Exception ex) {
+                out.println("Erro ao inserir: " + ex.getMessage());
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 15/04/2017, 17:55:53
+    Author     : Leandro David
+    Author     : Thayllan Siqueira
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
